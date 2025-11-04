0c8244a2ee661e5000d3fe84c2007e5c9f046ab4
Kevan <kevanpatira.cs24@bmsce.ac.in>
2025-11-04 18:23:38 +0530

    Add files via upload

---
+++ linked_list.c
@@ -0,0 +1,120 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *Head = NULL, *Temp = NULL, *Last = NULL, *new_node;
+void create_node(int val)
+{
+    new_node = (struct Node *) malloc(sizeof(struct Node));
+    new_node->data = val;
+    new_node->next = NULL;
+    return;
+}
+void insert_at_beg(int val)
+{
+    create_node(val);
+    if(Head == NULL)
+        Head = new_node;
+    else
+    {
+        new_node->next = Head;
+        Head = new_node;
+    }
+    printf("\n%d inserted at the front of list.\n",val);
+    return;
+}
+void insert_at_end(int val)
+{
+    create_node(val);
+    if(Head ==NULL)
+        Head = new_node;
+    else
+    {
+        Last = Head;
+        while(Last!=NULL)
+        {
+            Last = Last->next;
+        }
+        Last->next = new_node;
+    }
+    printf("\n%d inserted at the back of list.\n",val);
+    return;
+}
+void insert_at_pos(int val, int pos)
+{
+    if(pos == 0)
+    {
+        insert_at_beg(val);
+        return;
+    }
+    create_node(val);
+    if(Head == NULL)
+        Head = new_node;
+    else
+    {
+        Temp = Head;
+        for(int i = 0; i<pos-1;i++)
+        {
+            Temp = Temp->next;
+        }
+        new_node->next = Temp->next;
+        Temp->next = new_node;
+    }
+    printf("\n%d is inserted at %d index of list.\n", val, pos);
+    return;
+}
+void display()
+{
+    if(Head == NULL)
+    {
+        printf("Empty list.\n");
+        return;
+    }
+    else
+    {
+        Temp = Head;
+        while(Temp !=NULL)
+        {
+            printf("%d ,",Temp->data);
+            Temp = Temp->next;
+        }
+        printf("%d ,",Temp->data);
+    }
+    return;
+}
+void main()
+{
+    int ch, val,pos;
+    while(1)
+    {
+        printf("\n1. insert at front, 2. insert at back, 3.insert at a position, 4. display :");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:
+                printf("Enter value:");
+                scanf("%d", &val);
+                insert_at_beg(val);
+                break;
+            case 2:
+                printf("Enter value:");
+                scanf("%d", &val);
+                insert_at_end(val);
+                break;
+            case 3:
+                printf("Enter value and position:");
+                scanf("%d %d", &val,&pos);
+                insert_at_pos(val,pos);
+                break;
+            case 4:
+                display();
+                break;
+            default:
+                exit(0);
+        }
+    }
+    return;
+}
